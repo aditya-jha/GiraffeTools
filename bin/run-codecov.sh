5e8c70a849f544f846d92627fa5f1dfb0adc4ebf
Aditya Jha <aditya.jha@indifi.com>
2018-05-22 17:26:31 +0530

    changes

--- bin/run-codecov.sh
+++ bin/run-codecov.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # python tests
-echo $PWD
-cd app
-echo $PWD
 coverage run ./manage.py test --noinput --settings=app.settings.local
 bash <(curl -s https://codecov.io/bash) -cF python
 
 # javascript tests
 cd ..
-echo $PWD
 npm test
 bash <(curl -s https://codecov.io/bash) -cF javascript
